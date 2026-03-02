1f8e0b4fb4de6881f47ab31de5236bb3832a4f85
alejandroschmeil-lang <alejandro.schmeil@davinci.edu.ar>
2026-03-02 14:13:11 +0000

    Ayayaya

--- app/views/task_views_operations/task_batch_operations.py
+++
@@ -1,7 +0,0 @@
-def reasignar_tareas(task_tuples, new_owner):
-    for _, task in task_tuples:
-        task.owner = new_owner
-
-def eliminar_tareas_por_indices(tasks, indices):
-    for i in sorted(indices, reverse=True):
-        tasks.pop(i)
